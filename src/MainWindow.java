b3534030e21a07d3a9a2eca569645f806feb44f1
kaige_home <941274093@qq.com>
2022-04-30 22:19:21 +0800

    2022/4/30

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,4 +1,3 @@
-import java.awt.HeadlessException;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
@@ -8,16 +7,16 @@ public final class MainWindow extends JFrame {
 	private DrawPanel dPanel = new DrawPanel();// 绘图首选JPane类，实现了双缓冲
 	private Timer timer = new Timer(20, e -> dPanel.repaint());// 定时刷新,每20毫秒一次
 
-	public MainWindow() throws HeadlessException {
-		setLocation(400, 50);;
-		//setLocationRelativeTo(null);// 窗口居中显示
-		setResizable(false);
-		setTitle("坦克大战");
-		getContentPane().add(dPanel);//把窗体的默认根面板设置成dpanel
+	public MainWindow(){
+		setUndecorated(true);//去掉窗口所有的装饰，这句最好写在最前。
+		setResizable(false);//窗口不可调整。
+		getContentPane().add(dPanel);//把dpanel添加到窗体的根面板。
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		new ImageUtill();
-		setVisible(true);
 		pack();//一定要加这句。
-		timer.start();// 启动定时刷新，每20毫秒一次
+		setLocationRelativeTo(null);// 窗口居中显示。这句要加在窗口添加完所有组件并打包之后。
+		setTitle("坦克大战");
+		setVisible(true);
+		new ImageUtill();//加载资源文件。
+		timer.start();// 启动定时刷新dPanel，每20毫秒一次
 	}
 }
